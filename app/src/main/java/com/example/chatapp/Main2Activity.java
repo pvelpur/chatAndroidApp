aa8d2ee245a1efbf41f0a4250e556420afbcd819
pvelpur <pvelpur@purdue.edu>
2019-08-15 19:10:14 -0400

    Set up environment and enable phone authentication via Google firebase

---
+++ app/src/main/java/com/example/chatapp/Main2Activity.java
@@ -0,0 +1,36 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button mlogout = findViewById(R.id.logout);
+
+        mlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                //Makes sure to clear everything on top of it and kill any processes
+                //So user doesn't have access to anything other than login page
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
